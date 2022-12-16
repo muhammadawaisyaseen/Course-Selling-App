c14b5e64efbd1194b196640ba08762f54591cf0a
Muhammad Awais Yaseen <85793305+muhammadawaisyaseen@users.noreply.github.com>
2022-12-16 17:59:18 +0500

    circle Avatar

--- lib/screens/myHomePage.dart
+++ lib/screens/myHomePage.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -7,8 +10,24 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [],
+    return Scaffold(
+      backgroundColor: const Color(0xFF7fcae5),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
+              children: const [
+                CircleAvatar(
+                  backgroundColor: Color(0xFFcedfe0),
+                  radius: 30,
+                  backgroundImage: AssetImage("assets/images/awais.jpg"),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
