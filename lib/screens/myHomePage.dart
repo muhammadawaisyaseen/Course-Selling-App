20adf6330f3801f155329e10a3bd6a55cab833de
Muhammad Awais Yaseen <85793305+muhammadawaisyaseen@users.noreply.github.com>
2022-12-17 08:09:05 +0500

    progressbar_started

--- lib/screens/myHomePage.dart
+++ lib/screens/myHomePage.dart
@@ -29,43 +29,133 @@ List courses = [
   "Animation",
 ];
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFF7fcae5),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xFF7fcae5),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              children: [
-                const CircleAvatar(
-                  backgroundColor: Color(0xFFcedfe0),
-                  radius: 30,
-                  backgroundImage: AssetImage("assets/images/awais.jpg"),
-                ),
-                Spacer(),
-                CircularButton(icon: Icons.search),
-                const SizedBox(
-                  width: 16,
-                ),
-                CircularButton(icon: Icons.notifications),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  const CircleAvatar(
+                    backgroundColor: Color(0xFFcedfe0),
+                    radius: 30,
+                    backgroundImage: AssetImage("assets/images/awais.jpg"),
+                  ),
+                  Spacer(),
+                  CircularButton(icon: Icons.search),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  CircularButton(icon: Icons.notifications),
+                ],
+              ),
             ),
             const SizedBox(
               height: 40,
             ),
-            Expanded(
+            SizedBox(
+              height: 80,
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
                 itemCount: courses.length,
                 itemBuilder: (context, index) {
                   return HorizontalListview(
                     courseName: courses[index],
+                    ontap: () {
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                    },
+                    isSelected: selectedIndex == index,
                   );
                 },
+              ),
+            ),
+            SizedBox(
+              height: 35,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                "Popular Courses",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Container(
+                width: 400,
+                height: 300,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30)),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 20,
+                        right: 20,
+                        top: 30,
+                        bottom: 30,
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'UI/UX design Course',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                          ),
+                          Spacer(),
+                          Container(
+                            width: 35,
+                            height: 35,
+                            decoration: BoxDecoration(
+                              color: Colors.blueGrey,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Icon(Icons.more_horiz),
+                          ),
+                        ],
+                      ),
+                    ),
+                    
+                    // progress bar container
+                    Spacer(),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      width: 400,
+                      height: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(30)),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
--- lib/widgets/custom_listview.dart
+++ lib/widgets/custom_listview.dart
@@ -1,35 +1,42 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class HorizontalListview extends StatefulWidget {
-  HorizontalListview({required this.courseName, super.key});
+  HorizontalListview(
+      {required this.courseName,
+      required this.ontap,
+      required this.isSelected,
+      super.key});
 
-  String courseName;
+  final String courseName;
+  final VoidCallback ontap;
+  final bool isSelected;
 
   @override
   State<HorizontalListview> createState() => _HorizontalListviewState();
 }
 
 class _HorizontalListviewState extends State<HorizontalListview> {
-  bool myvalue = false;
+
 
   @override
   Widget build(BuildContext context) {
+    //print(widget.update);
     return UnconstrainedBox(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 4),
-        child: InkWell(
-          onTap: () {
-            setState(() {
-              myvalue = !myvalue;
-            });
-          },
+        child: GestureDetector(
+          onTap: widget.ontap,
           child: Container(
             // container size increase or decrease according to content
             decoration: BoxDecoration(
-                color: myvalue == false ? Color(0xFF7fcae5) : Colors.black,
+                color: widget.isSelected
+                    ? Colors.black
+                    : Color(0xFF7fcae5),
                 borderRadius: BorderRadius.circular(30),
                 border: Border.all(color: Colors.white, width: 1.5)),
             child: Center(
