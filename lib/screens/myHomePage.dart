d47924d99961fe95897dfcac705ea618029afe62
Muhammad Awais Yaseen <85793305+muhammadawaisyaseen@users.noreply.github.com>
2022-12-20 23:54:29 +0500

    working on slider

--- lib/screens/myHomePage.dart
+++ lib/screens/myHomePage.dart
@@ -88,7 +88,7 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             SizedBox(
-              height: 35,
+              height: 25,
             ),
             Padding(
               padding: const EdgeInsets.all(20),
@@ -100,9 +100,9 @@ class _HomeScreenState extends State<HomeScreen> {
                     fontSize: 18),
               ),
             ),
-            SizedBox(
-              height: 20,
-            ),
+            // SizedBox(
+            //   height: 20,
+            // ),
             Padding(
               padding: const EdgeInsets.all(12),
               child: Container(
@@ -123,7 +123,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       child: Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
+                          const Text(
                             'UI/UX design Course',
                             style: TextStyle(
                               color: Colors.black,
@@ -131,12 +131,12 @@ class _HomeScreenState extends State<HomeScreen> {
                               fontSize: 24,
                             ),
                           ),
-                          Spacer(),
+                          const Spacer(),
                           Container(
                             width: 35,
                             height: 35,
-                            decoration: BoxDecoration(
-                              color: Colors.blueGrey,
+                            decoration: const BoxDecoration(
+                              color: Colors.blueAccent,
                               shape: BoxShape.circle,
                             ),
                             child: Icon(Icons.more_horiz),
@@ -153,23 +153,23 @@ class _HomeScreenState extends State<HomeScreen> {
                         width: 400,
                         height: 180,
                         decoration: BoxDecoration(
-                            color: Colors.red,
+                            color: Color(0xFFf9f9f9),
                             borderRadius: BorderRadius.circular(30)),
                         child: Column(
                           children: [
                             Row(
-                              children: [
+                              children: const [
                                 Text("Progress"),
                                 Spacer(),
                                 Text("45%"),
                               ],
                             ),
-                            SizedBox(
+                            const SizedBox(
                               height: 20,
                             ),
                             // progress bar 5 small containers
                             Row(
-                              children: [
+                              children: const [
                                 ProgressBar(),
                                 Spacer(),
                                 ProgressBar(),
@@ -181,10 +181,10 @@ class _HomeScreenState extends State<HomeScreen> {
                                 ProgressBar(),
                               ],
                             ),
-                            Spacer(),
+                            const Spacer(),
                             // Time and date Row
                             Row(
-                              children: [
+                              children: const [
                                 Icon(Icons.access_time),
                                 SizedBox(
                                   width: 10,
@@ -195,7 +195,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                 SizedBox(
                                   width: 10,
                                 ),
-                                Text("26 Apr")
+                                Text("26 Apr"),
                               ],
                             )
                           ],
@@ -206,6 +206,53 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ),
+
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Container(
+                // padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                width: 400,
+                height: 120,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30)),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Row(
+                        children: [
+                          Text("UI Design"),
+                          Spacer(),
+                          Text("56%"),
+                        ],
+                      ),
+                    ),
+                    Spacer(),
+                    // Slider
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 6),
+                      child: SliderTheme(
+                        data: SliderThemeData(
+                          thumbShape:
+                              RoundSliderThumbShape(enabledThumbRadius: 0.0),
+                          trackHeight: 10,
+                        ),
+                        child: Slider(
+                          value: 0.5,
+                          onChanged: (value) {},
+                          activeColor: Color(0xFF7fcae5),
+                          inactiveColor: Color(0xFFf8f8f8),
+                        ),
+                      ),
+                    )
+                    // SliderThemeData(
+
+                    // )
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
