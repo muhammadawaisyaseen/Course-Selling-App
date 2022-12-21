cb712ec6fb6f4542417e9a49291b9eefe8b494d5
Muhammad Awais Yaseen <85793305+muhammadawaisyaseen@users.noreply.github.com>
2022-12-21 23:46:23 +0500

    update progressbar

--- lib/screens/myHomePage.dart
+++ lib/screens/myHomePage.dart
@@ -7,6 +7,7 @@ import 'package:flutter/src/widgets/framework.dart';
 
 import '../widgets/custom_listview.dart';
 import '../widgets/progressbar_small_containers.dart';
+import '../widgets/progressbar_small_containers.dart';
 import '../widgets/search_notif_widget.dart';
 
 List courses = [
@@ -104,181 +105,17 @@ class _HomeScreenState extends State<HomeScreen> {
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
+                  // mainAxisSize: MainAxisSize.max,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(12),
-                      child: Container(
-                        width: 400,
-                        height: 300,
-                        decoration: BoxDecoration(
-                            // color: Colors.white,
-                            gradient: const LinearGradient(
-                              colors: [
-                                Color(0xFFd8ffff),
-                                Color(0xFFfefdd9),
-                                Color(0xFFf9d3fe),
-                                Color(0xFFbafeff),
-                              ],
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                              stops: [0.05, 0.4, 0.7, 0.95],
-                            ),
-                            borderRadius: BorderRadius.circular(30)),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                left: 20,
-                                right: 20,
-                                top: 30,
-                                bottom: 30,
-                              ),
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  RichText(
-                                    text: const TextSpan(
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 24),
-                                        // DefaultTextStyle.of(context).style,
-                                        children: [
-                                          TextSpan(text: 'UI/UX'),
-                                          TextSpan(text: ' '),
-                                          TextSpan(
-                                              text: 'Design Course',
-                                              style: TextStyle(
-                                                color: Colors.black,
-                                              ))
-                                        ]),
-                                  ),
-                                  const Spacer(),
-                                  Container(
-                                    width: 35,
-                                    height: 35,
-                                    decoration: const BoxDecoration(
-                                      color: Color(0xFFffffff),
-                                      shape: BoxShape.circle,
-                                    ),
-                                    child: Icon(Icons.more_horiz),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Spacer(),
-                            // progress bar wala container
-                            Padding(
-                              padding: const EdgeInsets.all(6),
-                              child: Container(
-                                padding: EdgeInsets.all(30),
-                                width: 400,
-                                height: 180,
-                                decoration: BoxDecoration(
-                                    color: Color(0xFFf9f9f9),
-                                    borderRadius: BorderRadius.circular(30)),
-                                child: Column(
-                                  children: [
-                                    Row(
-                                      children: const [
-                                        Text("Progress"),
-                                        Spacer(),
-                                        Text("45%"),
-                                      ],
-                                    ),
-                                    const SizedBox(
-                                      height: 20,
-                                    ),
-                                    // progress bar 5 small containers
-                                    Row(
-                                      children: const [
-                                        ProgressBar(),
-                                        Spacer(),
-                                        ProgressBar(),
-                                        Spacer(),
-                                        ProgressBar(),
-                                        Spacer(),
-                                        ProgressBar(),
-                                        Spacer(),
-                                        ProgressBar(),
-                                      ],
-                                    ),
-                                    const Spacer(),
-                                    // Time and date Row
-                                    Row(
-                                      children: const [
-                                        Icon(Icons.access_time),
-                                        SizedBox(
-                                          width: 10,
-                                        ),
-                                        Text("6h 50min"),
-                                        Spacer(),
-                                        Icon(Icons.calendar_month_outlined),
-                                        SizedBox(
-                                          width: 10,
-                                        ),
-                                        Text("26 Apr"),
-                                      ],
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+                    firstContainer(),
                     ///////////////////////////////////////////////////
-                    Padding(
-                      padding: const EdgeInsets.all(12),
-                      child: Container(
-                        // padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                        width: 400,
-                        height: 120,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(30)),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(20),
-                              child: Row(
-                                children: const [
-                                  Text("UI Design"),
-                                  Spacer(),
-                                  Text("56%"),
-                                ],
-                              ),
-                            ),
-                            Spacer(),
-                            // Slider
-                            Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 6),
-                              child: SliderTheme(
-                                data: const SliderThemeData(
-                                  thumbShape: RoundSliderThumbShape(
-                                      enabledThumbRadius: 0.0),
-                                  trackHeight: 10,
-                                ),
-                                child: Slider(
-                                  value: 0.5,
-                                  onChanged: (value) {},
-                                  activeColor: Color(0xFF7fcae5),
-                                  inactiveColor: Color(0xFFf8f8f8),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
+                    secodContainer(),
                   ],
                 ),
               ),
             ),
           ],
         ),
-        // bottomNavigation Bar
         bottomNavigationBar: Padding(
           padding: const EdgeInsets.all(12),
           child: ClipRRect(
@@ -304,6 +141,174 @@ class _HomeScreenState extends State<HomeScreen> {
               selectedIndex: current_page,
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget secodContainer() {
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Container(
+        // padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        width: 400,
+        height: 120,
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(30)),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                children: const [
+                  Text("UI Design"),
+                  Spacer(),
+                  Text("56%"),
+                ],
+              ),
+            ),
+            Spacer(),
+            // Slider
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 6),
+              child: SliderTheme(
+                data: const SliderThemeData(
+                  thumbShape: RoundSliderThumbShape(enabledThumbRadius: 0.0),
+                  trackHeight: 10,
+                ),
+                child: Slider(
+                  value: 0.5,
+                  onChanged: (value) {},
+                  activeColor: Color(0xFF7fcae5),
+                  inactiveColor: Color(0xFFf8f8f8),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding firstContainer() {
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Container(
+        width: 400,
+        height: 300,
+        decoration: BoxDecoration(
+            // color: Colors.white,
+            gradient: const LinearGradient(
+              colors: [
+                Color(0xFFd8ffff),
+                Color(0xFFfefdd9),
+                Color(0xFFf9d3fe),
+                Color(0xFFbafeff),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              stops: [0.05, 0.4, 0.65, 1],
+            ),
+            borderRadius: BorderRadius.circular(30)),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+                top: 30,
+                bottom: 30,
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                    text: const TextSpan(
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24),
+                        // DefaultTextStyle.of(context).style,
+                        children: [
+                          TextSpan(text: 'UI/UX'),
+                          TextSpan(text: ' '),
+                          TextSpan(
+                              text: 'Design Course',
+                              style: TextStyle(
+                                color: Colors.black,
+                              ))
+                        ]),
+                  ),
+                  const Spacer(),
+                  Container(
+                    width: 35,
+                    height: 35,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFffffff),
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(Icons.more_horiz),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
+            // progress bar wala container
+            Padding(
+              padding: const EdgeInsets.all(6),
+              child: Container(
+                padding: EdgeInsets.all(30),
+                width: 400,
+                height: 180,
+                decoration: BoxDecoration(
+                    color: Color(0xFFf9f9f9),
+                    borderRadius: BorderRadius.circular(30)),
+                child: Column(
+                  children: [
+                    Row(
+                      children: const [
+                        Text("Progress"),
+                        Spacer(),
+                        Text("45%"),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ListView.builder(
+                        itemCount: 5,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return ProgressBarListview();
+                        },
+                      ),
+                    ),
+
+                    const Spacer(),
+                    // Time and date Row
+                    Row(
+                      children: const [
+                        Icon(Icons.access_time),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text("6h 50min"),
+                        Spacer(),
+                        Icon(Icons.calendar_month_outlined),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text("26 Apr"),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/widgets/progressbar_small_containers.dart
+++ lib/widgets/progressbar_small_containers.dart
@@ -1,17 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class ProgressBar extends StatelessWidget {
-  const ProgressBar({super.key});
+class ProgressBarListview extends StatelessWidget {
+  const ProgressBarListview({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 48,
-      height: 8,
-      decoration: BoxDecoration(
-          color: Colors.white, borderRadius: BorderRadius.circular(30)),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: UnconstrainedBox(
+        child: Container(
+          height: 6,
+          width: 46,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(30)),
+        ),
+      ),
     );
   }
 }
